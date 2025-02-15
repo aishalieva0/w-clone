adfc4ea7f6a06f1b2312a234eb8de5f7d6b6cec5
aishalieva0 <aysalieva6@gmail.com>
2025-02-16 03:23:17 +0400

    chatlist component

--- frontend/src/components/sidebar/ChatList.jsx
+++ frontend/src/components/sidebar/ChatList.jsx
@@ -1,20 +1,52 @@
 import React from "react";
+// import { ReactComponent as newChatIcon } from "../../assets/media/icons/newChat.svg";
+import { ReactComponent as NewChatIcon } from "../../assets/media/icons/newChat.svg";
+import { ReactComponent as MoreIcon } from "../../assets/media/icons/more.svg";
+import DefaultProfilePhoto from "../../assets/media/user/user-default.jpg";
 
 const ChatList = () => {
   return (
-    <ul className="chatList">
-      <li className="chatItem">
-        <div className="profileImg">photo</div>
-        <div className="content">
-          <h3>Jhon Doe</h3>
-          <p>Hey! Are you there?</p>
+    <div className="chatList">
+      <div className="container">
+        <div className="row">
+          <div className="chatHeader">
+            <div className="heading">
+              <h2>Chats</h2>
+            </div>
+            <div className="moreIcons">
+              <NewChatIcon className="icon" />
+              <MoreIcon className="icon" />
+            </div>
+          </div>
+          <div className="searchBar">
+            <input type="text" name="search" id="search" placeholder="Search" />
+          </div>
+          <div className="chatFilter">
+            <span>All</span>
+            <span>Unread</span>
+            <span>Favorites</span>
+            <span>Groups</span>
+          </div>
+          <ul className="conversationList">
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span>1</span>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div className="details">
-          <span>14:13</span>
-          <span>1</span>
-        </div>
-      </li>
-    </ul>
+      </div>
+    </div>
   );
 };
 
