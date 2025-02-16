995ca66fc5ecad7e4293b1076073de8763eab038
aishalieva0 <aysalieva6@gmail.com>
2025-02-17 01:49:54 +0400

    chatlist: styled filter, scrollbar, chats

--- frontend/src/components/sidebar/ChatList.jsx
+++ frontend/src/components/sidebar/ChatList.jsx
@@ -22,7 +22,7 @@ const ChatList = () => {
             <input type="text" name="search" id="search" placeholder="Search" />
           </div>
           <div className="chatFilter">
-            <span>All</span>
+            <span className="active">All</span>
             <span>Unread</span>
             <span>Favorites</span>
             <span>Groups</span>
@@ -39,7 +39,143 @@ const ChatList = () => {
                 </div>
                 <div className="details">
                   <span className="date">14:13</span>
-                  <span>1</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
+                </div>
+              </div>
+            </li>
+            <li className="chatItem">
+              <div className="profileImg">
+                <img src={DefaultProfilePhoto} alt="profile_photo" />
+              </div>
+              <div className="content">
+                <div className="info">
+                  <h3>Jhon Doe</h3>
+                  <p>Hey! Are you there?</p>
+                </div>
+                <div className="details">
+                  <span className="date">14:13</span>
+                  <span className="unread">1</span>
                 </div>
               </div>
             </li>
