f3e2b36c505f3f5e16ecc31fb75daad598d684d3
aishalieva0 <aysalieva6@gmail.com>
2025-03-21 03:34:33 +0400

    feat: change profile photo improved ui/added functionality

--- frontend/src/components/sidebar/Profile.jsx
+++ frontend/src/components/sidebar/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DefaultProfilePhoto from "../../assets/media/user/user-default.jpg";
 import CameraIcon from "../../assets/media/icons/camera.svg?react";
 import EmojiInput from "../../assets/media/icons/emojiInput.svg?react";
@@ -10,6 +10,29 @@ import Folder from "../../assets/media/icons/folder.svg?react";
 import Delete from "../../assets/media/icons/delete.svg?react";
 
 const Profile = () => {
+  const [optionVisible, setOptionVisible] = useState(false);
+  const [optionPosition, setOptionPosition] = useState({ top: 0, left: 0 });
+  const [showEditImg, setShowEditImg] = useState(false);
+  const optionRef = useRef(null);
+
+  const handleOptionClick = (e) => {
+    setOptionVisible(!optionVisible);
+    setOptionPosition({ top: e.clientY, left: e.clientX });
+  };
+  const handleClickOutside = (e) => {
+    if (optionRef.current && !optionRef.current.contains(e.target)) {
+      setOptionVisible(false);
+      setShowEditImg(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="profile">
       <div className="row">
@@ -18,32 +41,45 @@ const Profile = () => {
         </div>
         <div className="profileContent">
           <div className="profileImgContainer">
-            <div className="profileImg">
+            <div
+              className="profileImg"
+              ref={optionRef}
+              onMouseEnter={() => setShowEditImg(true)}
+              onMouseLeave={() => !optionVisible && setShowEditImg(false)}
+            >
               <img src={DefaultProfilePhoto} alt="Profile Image" />
+              <div
+                className={`editImg ${
+                  showEditImg || optionVisible ? "visible" : ""
+                }`}
+                onClick={handleOptionClick}
+              >
+                <CameraIcon className="icon" />
+                <span>Change Profile Photo</span>
+              </div>
+              <ul
+                className={`optionList ${optionVisible ? "visible" : ""}`}
+                style={{ top: optionPosition.top, left: optionPosition.left }}
+              >
+                <li className="optionItem">
+                  <View className="icon" />
+                  <span>View photo</span>
+                </li>
+                <li className="optionItem">
+                  <CameraOutline className="icon" />
+                  <span>Take photo</span>
+                </li>
+                <li className="optionItem">
+                  <Folder className="icon" />
+                  <span>Upload photo</span>
+                </li>
+                <hr className="divider" />
+                <li className="optionItem">
+                  <Delete className="icon" />
+                  <span>Remove photo</span>
+                </li>
+              </ul>
             </div>
-            <div className="editImg">
-              <CameraIcon className="icon" />
-              <span>Change Profile Photo</span>
-            </div>
-            <ul className="optionList">
-              <li className="optionItem">
-                <View className="icon" />
-                <span>View photo</span>
-              </li>
-              <li className="optionItem">
-                <CameraOutline className="icon" />
-                <span>Take photo</span>
-              </li>
-              <li className="optionItem">
-                <Folder className="icon" />
-                <span>Upload photo</span>
-              </li>
-              <hr className="divider" />
-              <li className="optionItem">
-                <Delete className="icon" />
-                <span>Remove photo</span>
-              </li>
-            </ul>
           </div>
           <div className="profileInfo">
             <div className="detailContainer">
@@ -53,7 +89,7 @@ const Profile = () => {
                 <Pencil className="icon icon-fixed" />
               </div>
               <div className="inputGroup">
-                <input type="text" maxlength="25" />
+                <input type="text" maxLength="25" />
                 <div className="btnGroup">
                   <span className="letterCount">25</span>
                   <div className="iconGroup">
@@ -73,7 +109,7 @@ const Profile = () => {
                 <Pencil className="icon icon-fixed" />
               </div>
               <div className="inputGroup">
-                <input type="text" maxlength="25" />
+                <input type="text" maxLength="25" />
                 <div className="btnGroup">
                   <span className="letterCount">25</span>
                   <div className="iconGroup">
