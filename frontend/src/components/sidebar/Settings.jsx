fc026e0507b95afcbafdca8dcdcf22064464fc0d
aishalieva0 <aysalieva6@gmail.com>
2025-04-19 14:22:50 +0400

    setting layout ui

--- frontend/src/components/sidebar/Settings.jsx
+++ frontend/src/components/sidebar/Settings.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import LogoutBtn from "../../assets/media/icons/logout.svg?react";
 import ChatIcon from "../../assets/media/icons/chatIcon.svg?react";
+import Chevron from "../../assets/media/icons/chevron.svg?react";
+import DefaultProfilePhoto from "../../assets/media/user/user-default.jpg";
 import { logOutUser } from "../../firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
-import DefaultProfilePhoto from "../../assets/media/user/user-default.jpg";
 import { setActiveTab } from "../../redux/slices/sidebarSlice";
 
 const Setting = () => {
@@ -31,11 +32,11 @@ const Setting = () => {
           <h2>Settings</h2>
         </div>
         <div className="settingContainer">
-          <div className="container">
-            <div
-              className="profileInfo"
-              onClick={() => dispatch(setActiveTab("profileTab"))}
-            >
+          <div
+            className="profileInfo"
+            onClick={() => dispatch(setActiveTab("profileTab"))}
+          >
+            <div className="container">
               <div className="profileImg">
                 <img
                   src={profileImage || DefaultProfilePhoto}
@@ -47,20 +48,41 @@ const Setting = () => {
                 <p>{email}</p>
               </div>
             </div>
-            <div className="settingList">
-              <button className="settingItem">
-                <ChatIcon className="icon" />
-                Theme
-              </button>
-              <button className="settingItem">
-                <ChatIcon className="icon" />
-                <span>Wallpaper</span>
-              </button>
-              <button className="settingItem logout" onClick={handleLogout}>
-                <LogoutBtn className="icon" />
-                <span>Log out</span>
-              </button>
-            </div>
+          </div>
+          <div className="settingList">
+            <button className="settingItem">
+              <div className="container">
+                <div className="row">
+                  <div className="settingItemInner">
+                    <ChatIcon className="icon" />
+                    <div className="settingText">
+                      <h4>Theme</h4>
+                      <span>Light mode</span>
+                    </div>
+                  </div>
+                  <Chevron className="icon chevron" />
+                </div>
+              </div>
+            </button>
+            <button className="settingItem">
+              <div className="container">
+                <div className="row">
+                  <div className="settingItemInner">
+                    <ChatIcon className="icon" />
+                    <h4 className="">Wallpaper</h4>
+                  </div>
+                  <Chevron className="icon chevron" />
+                </div>
+              </div>
+            </button>
+            <button className="settingItem logout" onClick={handleLogout}>
+              <div className="container">
+                <div className="row">
+                  <LogoutBtn className="icon" />
+                  <span>Log out</span>
+                </div>
+              </div>
+            </button>
           </div>
         </div>
       </div>
